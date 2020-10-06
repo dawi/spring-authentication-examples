dcab5789bd52d53dbd7b1f0c3eba24862466064e
Daniel Wilmer <d.wilmer@laudert.de>
2020-10-06 16:19:16 +0200

    Add Gradle configuration

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+// plugin management is required to support spring milestone versions
+pluginManagement {
+
+    repositories {
+        maven { url = uri("https://repo.spring.io/milestone") }
+        gradlePluginPortal()
+    }
+
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "org.springframework.boot") {
+                useModule("org.springframework.boot:spring-boot-gradle-plugin:${requested.version}")
+            }
+        }
+    }
+}
+
+rootProject.name = "spring-authentication-examples"
